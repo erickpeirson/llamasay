1ea71d3be24d6fb66c91ab1ba982f192e7f6e405
Erick <erick@anomalo.com>
2024-05-28 14:29:16 +0000

    llamaserv

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM alpine
-RUN apk add --no-cache perl
+
+RUN apk add --no-cache perl python3
+
+ENV PYTHONUNBUFFERED=1
 COPY llamasay /usr/local/bin/llamasay
+COPY llamaserv.py /usr/local/bin/llamaserv.py
 COPY chaos.llama /usr/local/share/llamas/default.llama
 ENTRYPOINT ["/usr/local/bin/llamasay"]
--- llamaserv.py
+++ llamaserv.py
@@ -1,12 +1,9 @@
-##
-## Docker Cow
-##
-$the_llama = <<EOC;
-    $thoughts
-     $thoughts
-      $thoughts
-EOC
-$the_llama .= <<'EOC';                                                    
+#!/usr/bin/env python3
+
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+LLAMA = """
                           ▓▓  ▓▓                                        
                         ▓▓░░▓▓░░▓▓                                      
                       ▓▓▓▓░░░░░░▓▓                                      
@@ -30,5 +27,21 @@ $the_llama .= <<'EOC';
                             ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      
                             ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      
                             ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      
-                              ▒▒  ▓▓      ▓▓  ▓▓                                                                                                                   
-EOC
+                              ▒▒  ▓▓      ▓▓  ▓▓     
+"""
+
+
+class WebRequestHandler(BaseHTTPRequestHandler):
+  
+    def do_GET(self):
+        source = ":".join(map(str, self.client_address))
+        target = ":".join(map(str, os.environ.get("POD_IP", self.server.server_address)))
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(f"\tSource: {source}\n\tTarget: {target}\n\n{LLAMA}".encode("utf-8"))
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 9000), WebRequestHandler)
+    print("Llamaserv ready!")
+    server.serve_forever()
